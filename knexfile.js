42ccc450996a92abe751449d2d2d789080258ec4
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-01-28 21:59:17 -0500

    sqlite is deleted

--- knexfile.js
+++ knexfile.js
@@ -3,39 +3,39 @@
 
 module.exports = {
 
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/recipe.db3",
-    },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-    pool: {
-      afterCreate: (conn, done) => {conn.run("PRAGMA foreign_keys = ON", done)},
-    },
-  },
+  // development: {
+  //   client: "sqlite3",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: "./data/recipe.db3",
+  //   },
+  //   migrations: {
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds",
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {conn.run("PRAGMA foreign_keys = ON", done)},
+  //   },
+  // },
 
-  testing: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/recipeTesting.db3"
-    },
-    migrations: { directory: "./data/migrations" },
-    seeds: {
-      directory: "./data/seeds"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done)
-      },
-    },
-  },
+  // testing: {
+  //   client: "sqlite3",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: "./data/recipeTesting.db3"
+  //   },
+  //   migrations: { directory: "./data/migrations" },
+  //   seeds: {
+  //     directory: "./data/seeds"
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       conn.run("PRAGMA foreign_keys = ON", done)
+  //     },
+  //   },
+  // },
 
   production: {
     client: 'pg',
